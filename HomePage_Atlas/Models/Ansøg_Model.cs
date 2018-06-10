41a086bb09d8ca80520d25f77cfaa92c3c51a8f8
Steffen Fonzie Hartung <sf.h@outlook.dk>
2018-06-11 00:11:40 +0200

    Added_Simpel form

---
+++ HomePage_Atlas/Models/Ansøg_Model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HomePage_Atlas.Models
+{
+    public class Ansøg_Model
+    {
+        public bool IsTrue { get { return true; } }
+        /* NAVN */
+        [Display(Name = "Fulde navn")]
+        [Required(ErrorMessage = "Udfyld venligst dit navn")]
+        public string Name { get; set; }
+         /* MAIL */
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Udfyld venligst din email")]
+        public string Email { get; set; }
+    }
+}

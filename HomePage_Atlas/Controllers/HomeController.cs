de31fbf1a0ce2b144f76bae11d6c3c56afff1e44
Steffen Fonzie Hartung <sf.h@outlook.dk>
2018-06-10 21:02:01 +0200

    Atlas base site

---
+++ HomePage_Atlas/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomePage_Atlas.Controllers
+{
+    public class HomeController : Controller
+    {
+        /* Forside */
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /* Information */
+        public ActionResult Information()
+        {
+            return View();
+        }
+        /* Information */
+        public ActionResult Events()
+        {
+            return View();
+        }
+        /* Information */
+        public ActionResult Galleri()
+        {
+            return View();
+        }
+        /* ANSØG*/
+        public ActionResult Ansøg()
+        {
+            return View();
+        }
+    }
+}
---
+++ HomePage_Atlas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HomePage_Atlas.MvcApplication" Language="C#" %>

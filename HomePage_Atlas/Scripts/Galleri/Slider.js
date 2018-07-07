f066bbac1004ecfe59df51c81cb7cbc5d23403b1
Steffen Fonzie Hartung <sf.h@outlook.dk>
2018-07-07 02:11:16 +0200

    remade navbarTop

---
+++ HomePage_Atlas/Scripts/Galleri/Slider.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    $('#lightSlider').lightSlider({
+        gallery: true,
+        item: 1,
+        loop: true,
+        slideMargin: 0,
+        thumbItem: 9
+    });
+    $('#lightSlider_2').lightSlider({
+        gallery: true,
+        item: 1,
+        loop: true,
+        slideMargin: 0,
+        thumbItem: 9
+    });
+    $('#lightSlider_3').lightSlider({
+        gallery: true,
+        item: 1,
+        loop: true,
+        slideMargin: 0,
+        thumbItem: 9
+    });
+    $('#lightSlider_4').lightSlider({
+        gallery: true,
+        item: 1,
+        loop: true,
+        slideMargin: 0,
+        thumbItem: 9
+    });
+});
